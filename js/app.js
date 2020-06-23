d1fcc2d95e0fc8b68606bfd1ff2f350b3baa733b
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2020-06-23 16:51:30 -0300

    Section 2, Section 3, Short Links functionality finished

--- js/app.js
+++ js/app.js
@@ -22,5 +22,61 @@ document.body.onload = () => {
     }
   });
 
-  console.log(sideDrawer);
+  const generateId = (length) => {
+    let result = '';
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  };
+
+  const shortButton = document.querySelector('#section-2 button');
+  const errorMessage = document.querySelector('.error');
+  const shortedLinks = document.querySelector('.shorted-links');
+
+  shortButton.addEventListener('click', () => {
+    const input = document.querySelector('#section-2 input');
+
+    const linkRegex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+
+    if (input.value && input.value.match(linkRegex)) {
+      const shortedLink = `https://rel.ink/${generateId(5)}`;
+
+      const item = document.createElement('li');
+      item.innerHTML = `<p>${input.value}</p>
+        <div>
+          <a href="#">${shortedLink}</a>
+          <button>Copy</button>
+        </div>`;
+
+      const button = item.querySelector('button');
+      button.addEventListener('click', () => {
+        var data = [
+          new ClipboardItem({
+            'text/plain': new Blob([shortedLink], { type: 'text/plain' })
+          })
+        ];
+        navigator.clipboard.write(data).then(
+          () => {
+            button.className = 'copied';
+            button.innerHTML = 'Copied!';
+          },
+          () => {
+            console.error('Unable to write to clipboard. :-(');
+          }
+        );
+      });
+
+      shortedLinks.appendChild(item);
+
+      input.className = '';
+      errorMessage.style.visibility = 'hidden';
+    } else {
+      input.className = 'invalid';
+      errorMessage.style.visibility = 'visible';
+    }
+  });
 };
